288e0af22b8715eca86ba4424eaad9f1eae7f0b7
naonao2323 <yanagidanaoki9900@gmail.com>
2024-09-25 23:40:16 +0900

    refactor(encoder): encode function

--- pkg/encoder/encoder.go
+++ pkg/encoder/encoder.go
@@ -1,6 +1,13 @@
 package encoder
 
-import "ecs-task-def-action/pkg/plovider/ecs"
+import (
+	"encoding/json"
+	"errors"
+
+	"ecs-task-def-action/pkg/plovider/ecs"
+
+	"gopkg.in/yaml.v2"
+)
 
 type Format int
 
@@ -23,10 +30,46 @@ func GetFormat(ext string) Format {
 	}
 }
 
-type EcsTaskEncoder interface {
-	Encode(in []byte, format Format) (*ecs.TaskDefinition, error)
+type EncodeTarget interface {
+	ecs.TaskDefinition | []ecs.ContainerDefinition
+}
+type Encoder[P EncodeTarget] interface {
+	Encode(in []byte, format Format) (*P, error)
 }
 
-type EcsContainerEncoder interface {
-	Encode(in []byte, format Format) (*[]ecs.ContainerDefinition, error)
+func Encode[P EncodeTarget](in []byte, format Format) (*P, error) {
+	switch format {
+	case Json:
+		def, err := EncodeJson[P](in)
+		if err != nil {
+			return nil, errors.New("fail to encode json file")
+		}
+		return def, nil
+	case Yaml:
+		def, err := EncodeYaml[P](in)
+		if err != nil {
+			return nil, errors.New("fail to encode yaml file")
+		}
+		return def, nil
+	default:
+		return nil, errors.New("unknown extension")
+	}
+}
+
+func EncodeJson[P EncodeTarget](in []byte) (*P, error) {
+	var def P
+	err := json.Unmarshal(in, &def)
+	if err != nil {
+		return nil, err
+	}
+	return &def, nil
+}
+
+func EncodeYaml[P EncodeTarget](in []byte) (*P, error) {
+	var def P
+	err := yaml.Unmarshal(in, &def)
+	if err != nil {
+		return nil, err
+	}
+	return &def, nil
 }
